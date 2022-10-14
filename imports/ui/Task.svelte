38b26730f55ac2f8ac27ff788e1d881fa8c17fd7
Charumathi Badrinath <charumathib@users.noreply.github.com>
2022-10-14 17:48:55 -0400

    Update Task.svelte

--- imports/ui/Task.svelte
+++ imports/ui/Task.svelte
@@ -1,10 +1,12 @@
+<!-- Svelte allows you to write JavaScript and HTML code -->
 <script>
   export let task;
 
+  // calls our internal setIsChecked API endpoint to toggle the checked state
   const toggleChecked = () =>
     Meteor.call('tasks.setIsChecked', task._id, !task.isChecked);
 
-
+  // calls our internal remove API endpoint to toggle the remove state
   const deleteThisTask = () => Meteor.call('tasks.remove', task._id);
 </script>
 
