c39ddffb8320ae8a59dc0e9f372592a7efccdac1
Charumathi Badrinath <charumathib@users.noreply.github.com>
2022-10-14 17:49:36 -0400

    Update tasksMethods.js

--- imports/api/tasksMethods.js
+++ imports/api/tasksMethods.js
@@ -6,16 +6,18 @@ Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
 
+    // insert a new document with the task description text and created at date into TasksCollection
     TasksCollection.insert({
       text,
       createdAt: new Date(),
     });
   },
 
+   // find and delete task from TasksCollection
   'tasks.remove'(taskId) {
     check(taskId, String);
 
-    const task = TasksCollection.findOne({ _id: taskId});
+    const task = TasksCollection.findOne({ _id: taskId });
 
     if (!task) {
       throw new Meteor.Error('Access denied.');
@@ -24,11 +26,12 @@ Meteor.methods({
     TasksCollection.remove(taskId);
   },
 
+   // find and toggle check (via the $set call) for task in TasksCollection
   'tasks.setIsChecked'(taskId, isChecked) {
     check(taskId, String);
     check(isChecked, Boolean);
 
-    const task = TasksCollection.findOne({ _id: taskId});
+    const task = TasksCollection.findOne({ _id: taskId });
 
     if (!task) {
       throw new Meteor.Error('Access denied.');
