f08ba5fabc4f8cee858d027eac12e9d836888abf
Charumathi Badrinath <charumathib@users.noreply.github.com>
2022-10-14 17:49:13 -0400

    Update TaskForm.svelte

--- imports/ui/TaskForm.svelte
+++ imports/ui/TaskForm.svelte
@@ -1,17 +1,18 @@
 <script>
-    // export let user = null;
     let newTask = '';
 
+    // if a new task has been inputted (value is bound to the input form) call the insert API function
     const handleSubmit = () => {
       if (!newTask) return;
 
       Meteor.call('tasks.insert', newTask);
 
-      // Clear form
+      // clear form
       newTask = '';
     }
 </script>
 
+<!-- form where users can type new tasks -->
 <form class="task-form" on:submit|preventDefault={handleSubmit}>
     <input
             type="text"
