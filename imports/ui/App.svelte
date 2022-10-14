489120452baba78f6a192a13c31dba96f41a69dd
Charumathi Badrinath <charumathib@users.noreply.github.com>
2022-10-14 17:48:30 -0400

    Update App.svelte

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -3,85 +3,72 @@
   import { Meteor } from 'meteor/meteor';
   import Task from './Task.svelte';
   import TaskForm from './TaskForm.svelte';
-  import LoginForm from './LoginForm.svelte';
 
   let hideCompleted = false;
-
   const hideCompletedFilter = { isChecked: { $ne: true } };
 
 
   let incompleteCount;
   let pendingTasksTitle = '';
   let tasks = [];
-  let user = null;
   let isLoading = true;
 
+  // subscribe to task database in Mongo
   const handler = Meteor.subscribe('tasks');
   $m: {
-    // user = "charumathib";
 
-    // if (user) {
+    isLoading = !handler.ready();
 
-        isLoading = !handler.ready();
+    // once the handler is ready get tasks from the database with no filters or a completed filter
+    tasks = TasksCollection.find(
+        hideCompleted ? hideCompletedFilter : {},
+            // sort by creation time
+            { sort: { createdAt: -1 } }
+        ).fetch();
 
-        // const userFilter = { userId: user._id };
-        const pendingOnlyFilter = { ...hideCompletedFilter};
+    // count the number of pending tasks
+    incompleteCount = TasksCollection.find(hideCompletedFilter).count();
 
+    pendingTasksTitle = `${
+        incompleteCount ? ` (${incompleteCount})` : ''
+    }`;
+  }
 
-        tasks = TasksCollection.find(
-                hideCompleted ? pendingOnlyFilter : {},
-                { sort: { createdAt: -1 } }
-            ).fetch();
-
-        incompleteCount = TasksCollection.find(pendingOnlyFilter).count();
-
-        pendingTasksTitle = `${
-          incompleteCount ? ` (${incompleteCount})` : ''
-        }`;
-    }
-//   }
-
+  // setter function on hideCompleted
   const setHideCompleted = value => {
     hideCompleted = value;
   };
-  const logout = () => Meteor.logout();
+
 </script>
 
-
 <div class="app">
-    <header>
-        <div class="app-bar">
-            <div class="app-header">
-                <h1>📝️ To Do List {pendingTasksTitle}</h1>
-            </div>
-        </div>
-    </header>
-
-    <div class="main">
-        <!-- {#if user}
-            <div class="user" on:click={logout}>
-                {user.username} 🚪
-            </div> -->
-
-            <TaskForm />
-
-            <div class="filter">
-                <button on:click={() => setHideCompleted(!hideCompleted)}>
-                    {hideCompleted ? 'Show All' : 'Hide Completed'}
-                </button>
-            </div>
-
-            {#if isLoading}
-                <div class="loading">loading...</div>
-            {/if}
-
-            <ul class="tasks">
-              {#each tasks as task (task._id)}
-                  <Task task={task} />
-              {/each}
-            </ul>
-        <!-- {:else}
-            <LoginForm />
-        {/if} -->
+  <header>
+    <div class="app-bar">
+      <div class="app-header">
+        <h1>📝️ To Do List {pendingTasksTitle}</h1>
+      </div>
     </div>
+  </header>
+  <div class="main">
+    <!-- task input component -->
+    <TaskForm />
+    <!-- filter button -->
+    <div class="filter">
+      <button on:click={() => setHideCompleted(!hideCompleted)}>
+        {hideCompleted ? 'Show All' : 'Hide Completed'}
+      </button>
+    </div>
+
+    <!-- if tasks are still being fetched, print a loading message -->
+    {#if isLoading}
+      <div class="loading">loading...</div>
+    {/if}
+
+    <!-- display all tasks using Svelte's for each syntax -->
+    <ul class="tasks">
+      {#each tasks as task (task._id)}
+        <Task {task} />
+      {/each}
+    </ul>
+  </div>
 </div>
